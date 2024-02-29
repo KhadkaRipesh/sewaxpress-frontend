0e7ab5db16c65b7ed731ba186007e0430fc94fcd
khadkaripesh <khadkanripesh@gmail.com>
2024-03-01 05:05:12 +0545

    category

--- src/Pages/Admin/Category.tsx
+++ src/Pages/Admin/Category.tsx
@@ -1,7 +1,75 @@
+import { motion } from 'framer-motion';
+import { Title } from '../../Components/common/Title';
+import { Button, Space, Table, TableProps } from 'antd';
+import { useEffect, useState } from 'react';
+import { BACKEND_URL } from '../../constants/constants';
+import axios from 'axios';
+
+interface DataType {
+  key: string;
+  name: string;
+}
+const columns: TableProps<DataType>['columns'] = [
+  {
+    title: 'Sn.',
+    dataIndex: 'sn',
+    render: (text, record, index) => index + 1,
+  },
+  {
+    title: 'Category Name',
+    dataIndex: 'category_name',
+    key: 'name',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: () => (
+      <Space size='middle'>
+        <Button>Edit</Button>
+        <Button danger>Delete</Button>
+      </Space>
+    ),
+  },
+];
+
 function Categories() {
+  const [categories, setCategories] = useState([]);
+  const [totalCategories, setTotalCategories] = useState(1);
+
+  useEffect(() => {
+    fetchCategories(1, 1);
+  }, []);
+
+  const fetchCategories = (page: number, limit: number) => {
+    axios
+      .get(`${BACKEND_URL}/category?page=${page}&limit=${limit}`)
+      .then((response) => {
+        setCategories(response.data.data.result);
+        setTotalCategories(response.data.data.totalCount);
+      })
+      .catch((error) => console.log(error.response));
+  };
   return (
     <>
-      <div className='container'>Category from here</div>
+      <div className='admin-container'>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 1 } }}
+        >
+          <Title title='Categories' />
+          <Table
+            columns={columns}
+            dataSource={categories}
+            rowKey={'id'}
+            pagination={{
+              total: totalCategories,
+              onChange: (page, pageSize) => {
+                fetchCategories(page, pageSize);
+              },
+            }}
+          />
+        </motion.div>
+      </div>
     </>
   );
 }
