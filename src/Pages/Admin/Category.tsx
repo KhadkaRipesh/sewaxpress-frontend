32e52f81c14248c663285829bba1caa796b18428
khadkaripesh <khadkanripesh@gmail.com>
2024-03-02 18:52:15 +0545

    pagination on category page

--- src/Pages/Admin/Category.tsx
+++ src/Pages/Admin/Category.tsx
@@ -5,42 +5,46 @@ import { useEffect, useState } from 'react';
 import { BACKEND_URL } from '../../constants/constants';
 import axios from 'axios';
 
-interface DataType {
-  key: string;
-  name: string;
-}
-const columns: TableProps<DataType>['columns'] = [
-  {
-    title: 'Sn.',
-    dataIndex: 'sn',
-    render: (text, record, index) => index + 1,
-  },
-  {
-    title: 'Category Name',
-    dataIndex: 'category_name',
-    key: 'name',
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: () => (
-      <Space size='middle'>
-        <Button>Edit</Button>
-        <Button danger>Delete</Button>
-      </Space>
-    ),
-  },
-];
-
 function Categories() {
   const [categories, setCategories] = useState([]);
   const [totalCategories, setTotalCategories] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+
+  interface DataType {
+    key: string;
+    name: string;
+  }
+  const columns: TableProps<DataType>['columns'] = [
+    {
+      title: 'Sn.',
+      dataIndex: 'sn',
+      render: (text, record, index) => (currentPage - 1) * pageSize + index + 1,
+    },
+    {
+      title: 'Category Name',
+      dataIndex: 'category_name',
+      key: 'name',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: () => (
+        <Space size='middle'>
+          <Button>Edit</Button>
+          <Button danger>Delete</Button>
+        </Space>
+      ),
+    },
+  ];
 
   useEffect(() => {
-    fetchCategories(1, 1);
+    fetchCategories(1, 10);
   }, []);
 
   const fetchCategories = (page: number, limit: number) => {
+    setCurrentPage(page);
+    setPageSize(limit);
     axios
       .get(`${BACKEND_URL}/category?page=${page}&limit=${limit}`)
       .then((response) => {
@@ -49,6 +53,7 @@ function Categories() {
       })
       .catch((error) => console.log(error.response));
   };
+
   return (
     <>
       <div className='admin-container'>
@@ -64,6 +69,7 @@ function Categories() {
             pagination={{
               total: totalCategories,
               onChange: (page, pageSize) => {
+                console.log(pageSize);
                 fetchCategories(page, pageSize);
               },
             }}
